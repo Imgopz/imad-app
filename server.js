471626d31cdaace829389938d0b82deb0c9e22ba
Imgopz <30651792+Imgopz@users.noreply.github.com>
2017-08-04 01:37:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/:articleName',function (req, res) {
     // articleName == article-one
     // articles[articleName] == {} content for the object one
-    var articleName = ref.param.articleName;
+    var articleName = ref.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 

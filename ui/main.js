84227fdcc9a4569769062d48187b7f28b49bcbea
Imgopz <30651792+Imgopz@users.noreply.github.com>
2017-08-06 04:05:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,25 @@
 //Counter Code
 var button = document.getElementById('counter');
-
-
+button.onclick = function() { 
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it into variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // not yet done
+    };
+    request.open('GET', 'http://rrgkmech.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+// Submit Name
 var submit = document.getElementById('submit_btn')
 submit.onclick = function() { 
     // Create a request object
@@ -24,11 +42,11 @@ submit.onclick = function() {
         }
         // not yet done
     };
-    
+
     // Make the request
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET', 'http://rrgkmech.imad.hasura-app.io/submit-name?name' + name,true);
+    request.open('GET', 'http://rrgkmech.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 };
 

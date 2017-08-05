5e086c3eafb428197cf3e58e9520dc085eea6b71
Imgopz <30651792+Imgopz@users.noreply.github.com>
2017-08-06 03:47:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,35 +10,22 @@ button.onclick = function() {
         if (request.readyState === XMLHttpRequest.DONE) {
             //Take some action
             if (request.status === 200) {
-                var counter =  request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i< names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
             }
         }
         // not yet done
     };
     
     // Make the request
-    request.open('GET', 'http://rrgkmech.imad.hasura-app.io/counter',true);
+    request.open('GET', 'http://rrgkmech.imad.hasura-app.io/submit-name?name' + name,true);
     request.send(null);
-    
-};
-
-// Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-    // Make a request to the server and send the name 
-    
-    // Capture a list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for (var i=0; i< names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
 };
 
 

a5286d8ae59f5bb1f84a973a2a1cf6137d09e722
Imgopz <30651792+Imgopz@users.noreply.github.com>
2017-08-06 02:34:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,16 @@
-console.log('Loaded!');
-
-// change the text of main-text div
-var element = document.getElementById('main-text'
-    );
-element.innerHTML = 'New Value';
-
-// move the image
-
-var img = document.getElementById('img');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    img.style.marginLeft = '100px';
-    var interval = setInterval(moveRight, 50);
-}
+//Counter Code
+var button = document.getElementById('counter');
+var counter = 0;
+botton.onclick = function() { 
+    // Make a req to the counter endpoint
+    
+    
+    // Capture the response and store it into variable
+    
+    // Rendring the variable in the correct span
+    counter = counter + 1;
+    var span = doucumnet.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+    
+};

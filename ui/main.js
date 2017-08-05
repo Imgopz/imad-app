1b3749acf4563423dcb638d571f7fae28ea8f92b
Imgopz <30651792+Imgopz@users.noreply.github.com>
2017-08-06 03:15:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,25 @@ button.onclick = function() {
     request.send(null);
     
 };
+
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value();
+var submit=document.getElementById('submit_btn');
+Submit.onclick = function() {
+    // Make a request to the server and send the name 
+    
+    // Capture a list of names and render it as a list
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for (var i=0; i< names.lengt; i++) {
+        list += '<li>' + names [i] + '</li>';
+    }
+    var ul = document.getElementById('namelsit');
+    ul.innerHTML = list;
+};
+
+
+
+
+

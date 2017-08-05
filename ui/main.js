420186883c2743a8290757f2aaa1787c49891167
Imgopz <30651792+Imgopz@users.noreply.github.com>
2017-08-06 03:17:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,15 +27,15 @@ button.onclick = function() {
 // Submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value();
-var submit=document.getElementById('submit_btn');
-Submit.onclick = function() {
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
     // Make a request to the server and send the name 
     
     // Capture a list of names and render it as a list
     var names = ['name1','name2','name3','name4'];
     var list = '';
-    for (var i=0; i< names.lengt; i++) {
-        list += '<li>' + names [i] + '</li>';
+    for (var i=0; i< names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelsit');
     ul.innerHTML = list;

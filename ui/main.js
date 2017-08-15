ecfdac0fbcf457048c83957dd6b194cd616180df
Imgopz <30651792+Imgopz@users.noreply.github.com>
2017-08-16 03:37:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,8 +29,3 @@ submit.onclick = function() {
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username: username, password: password}));
 };
-
-
-
-
-

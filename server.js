b6071bc117936fe3adc326ebfec3d02758ffb513
Imgopz <30651792+Imgopz@users.noreply.github.com>
2017-08-16 00:49:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/login', function(req, res){
   });
 });
 
-app.get('/check-login', function(req,res){
+app.get('/check-login', function(req, res){
    if (req.session && req.session.auth && req.session.auth.userId) {
        res.send('You are logged in: ' + req.session.auth.userID.toString());
    } else {

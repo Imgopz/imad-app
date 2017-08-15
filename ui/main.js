24250616361d5bf4f3df01caa74a874f4f7dd804
Imgopz <30651792+Imgopz@users.noreply.github.com>
2017-08-16 00:33:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,5 @@
-//Counter Code
-var button = document.getElementById('counter');
-button.onclick = function() { 
-    // Create a request object
-    var request = new XMLHttpRequest();
-    
-    // Capture the response and store it into variable
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            //Take some action
-            if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        // not yet done
-    };
-    request.open('GET', 'http://rrgkmech.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-// Submit Name
+
+// Submit username & Password to login 
 var submit = document.getElementById('submit_btn')
 submit.onclick = function() { 
     // Create a request object
@@ -30,24 +10,25 @@ submit.onclick = function() {
         if (request.readyState === XMLHttpRequest.DONE) {
             //Take some action
             if (request.status === 200) {
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i< names.length; i++) {
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
+                console.log('user logged in successfully');
+                alert('logged in successfully');
+            } else if (request.status === 403) {
+                alert('Username/Password is incorrect');
+            } else if (request.status === 500) {
+                alert("Something went wrong on the server");
             }
-        }
+        }   
         // not yet done
     };
 
     // Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://rrgkmech.imad.hasura-app.io/submit-name?name=' + name,true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://rrgkmech.imad.hasura-app.io/login',true);
+    requset.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.strigify({username: username, password: password}));
 };
 
 

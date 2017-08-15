ab2cad2b559985700ab30e845596236c114d7c9f
Imgopz <30651792+Imgopz@users.noreply.github.com>
2017-08-16 03:39:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,5 +27,5 @@ submit.onclick = function() {
     console.log(password);
     request.open('POST', 'http://rrgkmech.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:username, password:password}));
+    request.send(JSON.stringify({username: username, password: password}));
 };
